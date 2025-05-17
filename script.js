24b7b5c6f13bee3b81d13e2ba7ce9013f6225c63
kalyanmondal2000 <kalyan.x.mondal@gmail.com>
2025-05-16 23:47:46 -0400

    debugging imports

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ import { GLTFLoader } from "../three/examples/jsm/loaders/GLTFLoader.js";
 import { GUI } from '/lil-gui.module.min.js';
 import { OrbitControls } from "../three/examples/jsm/controls/OrbitControls.js";
 import { Sky } from "../three/examples/jsm/objects/Sky.js";
-
+/*
 import GerstnerWater from "/gerstnerWater.js";
-import Floater from "/floater.js";
+import Floater from "/floater.js";*/
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
